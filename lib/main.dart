ab96052c9c31d2811f19cf7250544025a81290a8
Vinícius Marcili <vinipetter@gmail.com>
2020-03-05 20:55:12 -0300

    Cap 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,22 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(Text('Olá mundo', textDirection: TextDirection.ltr));
+void main() => runApp(
+  Column(
+    children: <Widget>[
+      Text(
+        'Oie',
+        textDirection: TextDirection.ltr,
+      ),
+      Text(
+        'Doido',
+        textDirection: TextDirection.ltr,
+      ),
+      Expanded(
+        child: FittedBox(
+          fit: BoxFit.contain,
+          child: const FlutterLogo(),
+        ),
+      ),
+    ],
+  )
+);
